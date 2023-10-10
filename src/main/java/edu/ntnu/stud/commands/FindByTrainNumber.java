1a135cb9f6daf2e8d0079e3a654d461a0545bcdd
Anders <anders.morille@gmail.com>
2023-10-10 21:43:43 +0200

    Renamed find by train

--- src/main/java/edu/ntnu/stud/commands/FindByTrainNumber.java
+++ src/main/java/edu/ntnu/stud/commands/FindByTrainNumber.java
@@ -3,14 +3,14 @@ package edu.ntnu.stud.commands;
 import edu.ntnu.stud.TrainDispatchApp;
 import edu.ntnu.stud.utils.UserInputHandler;
 
-public class SearchCommand extends Command {
-  public SearchCommand() {
-    super("search", "Search for a train departure by train number");
+public class FindByTrainNumber extends Command {
+  public FindByTrainNumber() {
+    super("find by num", "Find a train departure by train number");
   }
 
   @Override
   public void execute(TrainDispatchApp app) {
-    System.out.println("Search for train departure");
+    System.out.println("Find a train departure by train number");
 
     var input = new UserInputHandler(app);
     var departure = input.getDeparture();
