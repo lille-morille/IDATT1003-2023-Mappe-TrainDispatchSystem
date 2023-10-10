fe289fddfd49148aef1ea8ba73f7a6c3b386a82b
Anders <anders.morille@gmail.com>
2023-10-10 21:44:36 +0200

    You can now use numbers for commands

--- src/main/java/edu/ntnu/stud/TrainDispatchApp.java
+++ src/main/java/edu/ntnu/stud/TrainDispatchApp.java
@@ -3,11 +3,14 @@ package edu.ntnu.stud;
 import edu.ntnu.stud.commands.AddCommand;
 import edu.ntnu.stud.commands.Command;
 import edu.ntnu.stud.commands.ExitCommand;
+import edu.ntnu.stud.commands.FindByDestinationCommand;
 import edu.ntnu.stud.commands.PrintDeparturesCommand;
-import edu.ntnu.stud.commands.SearchCommand;
+import edu.ntnu.stud.commands.FindByTrainNumber;
+import edu.ntnu.stud.commands.SetClockCommand;
 import edu.ntnu.stud.commands.SetDelayCommand;
 import edu.ntnu.stud.commands.SetTrackCommand;
 import edu.ntnu.stud.models.TrainDeparture;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -31,18 +34,27 @@ public class TrainDispatchApp {
       new AddCommand(),
       new SetDelayCommand(),
       new SetTrackCommand(),
-      new SearchCommand(),
+      new FindByTrainNumber(),
+      new FindByDestinationCommand(),
+      new SetClockCommand(),
       new ExitCommand(),
   };
 
   private void runCommand(String command) {
-    Arrays.stream(COMMANDS)
-        .filter(c -> c.getName().equals(command))
-        .findFirst()
-        .ifPresentOrElse(
-            c -> c.execute(this),
-            () -> System.out.println("Unknown command: " + command)
-        );
+    try {
+      // Try to parse the command as a number
+      int commandIndex = Integer.parseInt(command) - 1;
+      COMMANDS[commandIndex].execute(this);
+    } catch (NumberFormatException ignored) {
+      // If that did not work, try the command name instead
+      Arrays.stream(COMMANDS)
+          .filter(c -> c.getName().equals(command))
+          .findFirst()
+          .ifPresentOrElse(
+              c -> c.execute(this),
+              () -> System.out.println("Unknown command: " + command)
+          );
+    }
   }
 
 
@@ -50,6 +62,7 @@ public class TrainDispatchApp {
     System.out.println("Starting train dispatch application...");
     // TODO remove sample data for production
     departures.addAll(getSampleDepartures());
+    this.clock = LocalTime.of(0, 0);
   }
 
   private void start() {
@@ -57,9 +70,10 @@ public class TrainDispatchApp {
     // noinspection InfiniteLoopStatement
     while (true) {
       System.out.println("============================");
-      System.out.println("Enter a command:");
-      for (Command command : COMMANDS) {
-        System.out.println(command.getName() + " - " + command.getDescription());
+      System.out.println("Enter a command (name or number):");
+      for (int i = 0; i < COMMANDS.length; i++) {
+        Command command = COMMANDS[i];
+        System.out.printf("(%d) %s - %s%n", i + 1, command.getName(), command.getDescription());
       }
       System.out.println();
       System.out.print("> ");
@@ -122,6 +136,24 @@ public class TrainDispatchApp {
   }
 
   private final List<TrainDeparture> departures = new ArrayList<>();
+
+  private LocalTime clock;
+
+  public LocalTime getClock() {
+    return clock;
+  }
+
+  /**
+   * Updates the applications clock.
+   * Also removes departures that have already departed.
+   */
+  public void setClock(LocalTime clock) {
+    // Add 1 second for departures with the same time to be removed
+    this.clock = clock.plusSeconds(1);
+
+    // Remove departures before this time
+    departures.removeIf(d -> d.getFinalDepartureTime().isBefore(clock));
+  }
 
   private List<TrainDeparture> getSampleDepartures() {
     return Arrays.stream(new TrainDeparture[] {
