6aca5cc3f9c32493efd1ed2389c33772c1a15bf4
Anders <anders.morille@gmail.com>
2023-11-23 10:46:03 +0100

    Removed test-command

--- src/main/java/edu/ntnu/stud/TrainDispatchApp.java
+++ src/main/java/edu/ntnu/stud/TrainDispatchApp.java
@@ -6,7 +6,6 @@ import edu.ntnu.stud.commands.ExitCommand;
 import edu.ntnu.stud.commands.FindByDestinationCommand;
 import edu.ntnu.stud.commands.FindByTrainNumberCommand;
 import edu.ntnu.stud.commands.PrintDeparturesCommand;
-import edu.ntnu.stud.commands.SayHiCommand;
 import edu.ntnu.stud.commands.SetClockCommand;
 import edu.ntnu.stud.commands.SetDelayCommand;
 import edu.ntnu.stud.commands.SetTrackCommand;
